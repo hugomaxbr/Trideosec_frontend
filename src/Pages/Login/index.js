79d7cb55df6578be567ddbf504b853c1f1d125c7
Hugo <hugofranco_12@hotmail.com>
2020-08-28 04:55:27 -0300

    Formulário de LogIn adicionado

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Form, Field } from './styles';
+import { Button } from '../../Components/Button/styles';
 
 function Login() {
-  return <div />;
+  return (
+    <>
+      <Container>
+        <Form action="/login">
+          <Field>
+            <input
+              placeholder=" "
+            />
+            <label for="email" class="label">Email</label>
+          </Field>
+          <Field>
+            <input
+              placeholder=" "
+            />
+            <label for="password" class="label">Senha</label>
+          </Field>
+
+          <Button type="submit">Entrar</Button>
+        </Form>
+
+      </Container>
+    </>
+  );
 }
 
 export default Login;
