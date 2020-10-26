6ffcc987ba59461228a54832e993dffe882c576e
Hugo Franco Alves Gil <hugofranco_12@hotmail.com>
2020-10-26 10:21:23 -0300

    i dunno, send help

--- src/Pages/Login/styles.js
+++ src/Pages/Login/styles.js
@@ -24,10 +24,6 @@ export const Container = styled.div`
     display: block;
     font-size: 1.2rem;
     color: white;
-<<<<<<< HEAD
-  };
-  
-=======
   }
 
   label{
@@ -59,7 +55,6 @@ export const Form = styled.form`
   padding: 15px 46px;
   font-size: 24px;
   font-weight: 700;
->>>>>>> 63d2daaa555f062c603bbbbc4c0a94510102c272
   
   Button{
     margin-top: 50px;
@@ -89,32 +84,7 @@ export const Field = styled.div`
     top: 2px;
   }
 
-<<<<<<< HEAD
-  label{
-    position: absolute;
-    transform: translateY(-2rem);
-    transform-origin: 0%;
-    transition: transform 400ms;
-  }
-
-  input{
-    outline: none;
-    border: none;
-    overflow: hidden;
-    margin: 0 auto;
-    width: 100%;
-    padding: 0.25rem 0;
-    background: none;
-    color: white;
-    font-size: 1.2rem;
-    font-weight: bold;
-  }
-
-
-  &:focus-within{
-=======
   &:focus-within {
->>>>>>> 63d2daaa555f062c603bbbbc4c0a94510102c272
     border-color: transparent;
   }
 
@@ -122,12 +92,7 @@ export const Field = styled.div`
     transform: scaleX(1);
   }
 
-<<<<<<< HEAD
-  .&:focus-within .label,
-.input:not(:placeholder-shown) + .label {
-=======
   &:focus-within label, input:not(:placeholder-shown) + label {
->>>>>>> 63d2daaa555f062c603bbbbc4c0a94510102c272
   transform: scale(0.8) translateY(-5rem);
   
 }
